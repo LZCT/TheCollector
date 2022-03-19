ef92220673a0537040a5c9f458fcfdb49a90fd3d
Leonardo <leocastabelli@gmail.com>
2022-03-19 05:35:23 -0300

    Change functions and output

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 // YOUR POKEMON TCG API KEY HERE
 const POKETCGAPI_KEY = ``;
 
-class Pokemon {
-    constructor(name, type, img, set,){}
-}
+const cardList = document.getElementById("cardList");
+
+
+class pokemonCard {
+    constructor(name, type, img, set,){
+        this._name = name;
+        this._type = type;
+        this._img = img;
+        this._set = set;
+
+    }
+};
 
 
 
 const fetchCard = () => {
-    let cardName = document.getElementById('cardName').value;
+   
+    let cardName = document.getElementById('cardName').value; 
     console.log(`CONSOLE: ${cardName}`)
+
     const url = `https://api.pokemontcg.io/v2/cards?q=name:${cardName}`;
     fetch(url, {
         method: "GET",
@@ -22,30 +33,50 @@ const fetchCard = () => {
         .then( res => res.json())
         .then((cards) => {
             console.log(cards.data)
-            const arrayCardsImages = []
+
+            let arrayCardsImages = []
             
             cards.data.forEach( (card) => {
                 console.log(card.name);
 
-                arrayCardsImages.push(card.images.large)
-
-                const img = new Image(300,420);
-                img.src = card.images.large;
-                document.body.appendChild(img);
+               arrayCardsImages.push(card.images.large)
+               
+                //const img = new Image(300,420);
+                //img.src = card.images.large;
+                //document.body.appendChild(img);
             });     
            // document.getElementById("preview-card").src = card[0].data.images.small;
+            for(let i= 0; i < arrayCardsImages.length; i++){
+                console.log(arrayCardsImages[i]);
+            }
+            
+            listCards(arrayCardsImages);
+
             
         })
             
 
     document.getElementById("txt").value = cardName;
     
-}
+};
 
 
-const teste = () => {
-    console.log("Funciona");
-}
+
+const listCards = (card) => {
+    
+    const listOfImages =  card.map( (cardImg) => 
+        `
+        <li class="card">
+        <img class="card-image" src="${cardImg}"/>
+        <li>
+        `
+    ).join('');
+
+    
+
+    cardList.innerHTML = listOfImages;
+
+};
 
 
 
